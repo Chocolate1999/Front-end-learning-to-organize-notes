e998c634aa5802fe7a42de71017d96496c351228
Chocolate1999 <2816778271@qq.com>
2020-09-13 23:23:57 +0800

    mark

---
+++ JS/基础知识/显示、隐式类型转换.js
@@ -0,0 +1,38 @@
+// console.log(typeof(1 -'1'))
+// console.log(typeof('1' - '1'))
+
+// console.log(a)
+
+// console.log(typeof(a))
+
+// console.log(typeof(null)) // object
+// console.log(typeof(undefined)) // undefined
+
+// console.log(typeof(typeof(a)))
+// console.log(typeof(typeof(123)))
+
+// let a = 'true'
+// console.log(typeof(Number(a)) + '-' + Number(a))
+
+// let a = true
+// console.log(typeof(Number(a)) + '-' + Number(a))
+
+// let a = null
+// console.log(typeof(Number(a)) + '-' + Number(a))
+
+// let a = undefined
+// console.log(typeof(Number(a)) + '-' + Number(a))
+
+// var a = 'a'
+// console.log(typeof(Number(a)) + '-' + Number(a)) // number-NaN
+// var a = '1a'
+// console.log(typeof(Number(a)) + '-' + Number(a))// number-NaN
+// var a = 3.14
+// console.log(typeof(Number(a)) + '-' + Number(a))// number-3.14
+// console.log(typeof(NaN)) // number
+
+// let a = '123'
+// console.log(typeof(parseInt(a)) + '-' + parseInt(a))
+
+let a = true
+console.log(typeof(parseInt(a)) + '-' + parseInt(a)) //number-NaN
