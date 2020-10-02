a9d04834474ae5ffbb5b0f03d8f90bed7046d4d5
Chocolate1999 <2816778271@qq.com>
2020-10-02 15:42:09 +0800

    使用antd设计todolist

--- 学习React/Redux学习/demo01/src/TodoList.js
+++ 学习React/Redux学习/demo01/src/TodoList.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd'
+
+const data = [
+  '写代码',
+  '吃饭',
+  '打游戏'
+]
+
 class TodoList extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {}
   }
-  render() { 
-    return (  
-      <div>Hello Chocolate!</div>
+  render() {
+    return (
+      <div style={{ margin: '10px' }}>
+        <div>
+          <Input
+            placeholder='Write Something'
+            style={{ width: '250px', marginRight: '10px' }}
+          />
+          <Button type="primary">增加</Button>
+        </div>
+        <div style={{ margin: '10px', width: '300px' }}>
+          <List
+            bordered
+            dataSource={data}
+            renderItem={item => (<List.Item>{item}</List.Item>)}
+          ></List>
+        </div>
+      </div>
     );
   }
 }
- 
+
 export default TodoList;
