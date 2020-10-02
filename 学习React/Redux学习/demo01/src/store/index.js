3085fd5a0351dec2137ef111d6ade9525cc0ea9c
Chocolate1999 <2816778271@qq.com>
2020-10-02 16:03:42 +0800

    redux tool

--- 学习React/Redux学习/demo01/src/store/index.js
+++ 学习React/Redux学习/demo01/src/store/index.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux'
+import { createStore } from 'redux' //  引入createStore方法
 import reducer from './reducer'
-const store = createStore(reducer);
-export default store;
+const store = createStore(reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) // 创建数据存储仓库
+  export default store   //暴露出去
