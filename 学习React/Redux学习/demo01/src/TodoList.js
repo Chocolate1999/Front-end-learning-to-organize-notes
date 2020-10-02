0faaa5bae7474bfa3b29a94246facc4d1bdfd06d
Chocolate1999 <2816778271@qq.com>
2020-10-02 15:31:40 +0800

    学习redux 初始化项目

---
+++ 学习React/Redux学习/demo01/src/TodoList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  
+      <div>Hello Chocolate!</div>
+    );
+  }
+}
+ 
+export default TodoList;
---
+++ 学习React/Redux学习/demo01/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './TodoList'
+
+ReactDOM.render(
+  <TodoList />,
+  document.getElementById('root')
+);
