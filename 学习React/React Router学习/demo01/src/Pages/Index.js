27ec0dec3129f9fe025fd103ecbecdb2a5ba32aa
Chocolate1999 <2816778271@qq.com>
2020-10-03 10:58:07 +0800

    React Router动态传参2

--- 学习React/React Router学习/demo01/src/Pages/Index.js
+++ 学习React/React Router学习/demo01/src/Pages/Index.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-
+import { Link } from "react-router-dom";
 class Index extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [
+        { uid: 123, title: '超逸の技术博客' },
+        { uid: 456, title: '一百个Chocolate' },
+        { uid: 789, title: '学如逆水行舟，不进则退' },
+      ]
     }
-    render() { 
-        return (  <h2>JSPang.com</h2> );
-    }
+  }
+  render() {
+    return (
+      <ul>
+        {
+          this.state.list.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link to={'/list/' + item.uid}> {item.title}</Link>
+              </li>
+            )
+          })
+        }
+      </ul>
+    );
+  }
 }
 
 export default Index;
