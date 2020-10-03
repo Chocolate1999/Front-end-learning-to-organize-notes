22834b790fe8d447f64770b93c5cb97f0ac6db3f
Chocolate1999 <2816778271@qq.com>
2020-10-03 15:28:17 +0800

    使用React Hooks

--- 学习React/React Hooks学习/demo01/src/Example.js
+++ 学习React/React Hooks学习/demo01/src/Example.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-function Example(){
-    const [ count , setCount ] = useState(0);
-    return (
-        <div>
-            <p>You clicked {count} times</p>
-            <button onClick={()=>{setCount(count+1)}}>click me</button>
-        </div>
-    )
+function Example() {
+  const [count, setCount] = useState(0); // 数组解构
+  const [age, setAge] = useState(21);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => { setCount(count + 1) }}>click me</button>
+      <p>年龄{age}</p>
+    </div>
+  )
 }
 export default Example;
