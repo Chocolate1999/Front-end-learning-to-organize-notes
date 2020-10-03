c442a109837de8d5e628c6be899e8fa30521bc73
Chocolate1999 <2816778271@qq.com>
2020-10-03 09:23:39 +0800

    redux中间件的安装和配置

--- 学习React/Redux学习/demo01/src/store/index.js
+++ 学习React/Redux学习/demo01/src/store/index.js
@@ -1,5 +1,12 @@
-import { createStore } from 'redux' //  引入createStore方法
-import reducer from './reducer'
-const store = createStore(reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) // 创建数据存储仓库
-  export default store   //暴露出去
+import { createStore, applyMiddleware, compose } from 'redux'; //  引入createStore方法
+import reducer from './reducer';
+import thunk from 'redux-thunk';
+
+// 利用compose创造一个增强函数，就相当于建立了一个链式函数
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
+
+const enhancer = composeEnhancers(applyMiddleware(thunk))
+
+const store = createStore( reducer, enhancer) // 创建数据存储仓库
+export default store   //暴露出去
