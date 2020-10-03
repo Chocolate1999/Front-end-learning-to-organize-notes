b04b1b63fdc87b3464bf5efe9dd0e8cbe3700ecd
Chocolate1999 <2816778271@qq.com>
2020-10-03 15:47:22 +0800

    React Hooks 实现componentWillUnmount

--- 学习React/React Hooks学习/demo01/src/Example.js
+++ 学习React/React Hooks学习/demo01/src/Example.js
@@ -1,14 +1,46 @@
-import React, { useState , useEffect } from 'react';
-function Example(){
-    const [ count , setCount ] = useState(0);
-    useEffect(()=>{
-        console.log(`useEffect=>You clicked ${count} times`)
-    })
-    return (
-        <div>
-            <p>You clicked {count} times</p>
-            <button onClick={()=>{setCount(count+1)}}>click me</button>
-        </div>
-    )
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+  useEffect(() => {
+    console.log('useEffect=>老弟，你来了！Index页面')
+    return () => {
+      console.log('老弟，你走了!Index页面')
+    }
+  }, [])
+  return <h2>yangchaoyi.vip</h2>;
+}
+
+function List() {
+  useEffect(() => {
+    console.log('useEffect=>老弟，你来了！List页面')
+  })
+  return <h2>List-Page</h2>;
+}
+
+
+function Example() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log(`useEffect=>You clicked ${count} times`);
+    return () => {
+      console.log('====================')
+    }
+  },[count])
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => { setCount(count + 1) }}>click me</button>
+
+      <Router>
+        <ul>
+          <li> <Link to="/">首页</Link> </li>
+          <li><Link to="/list/">列表</Link> </li>
+        </ul>
+        <Route path="/" exact component={Index} />
+        <Route path="/list/" component={List} />
+      </Router>
+    </div>
+  )
 }
 export default Example;
