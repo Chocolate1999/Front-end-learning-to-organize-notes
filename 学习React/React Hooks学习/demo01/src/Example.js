aaaafcee7f139fb4376ecd3095ac70d4d3478536
Chocolate1999 <2816778271@qq.com>
2020-10-03 15:19:38 +0800

    React Hooks 初识 搭建环境

---
+++ 学习React/React Hooks学习/demo01/src/Example.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+function Example(){
+    const [ count , setCount ] = useState(0);
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={()=>{setCount(count+1)}}>click me</button>
+        </div>
+    )
+}
+export default Example;
---
+++ 学习React/React Hooks学习/demo01/src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Example from './Example';
+ReactDOM.render(<Example />, document.getElementById('root'));
