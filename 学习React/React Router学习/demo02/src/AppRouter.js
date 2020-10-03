548ad7fda5893ff59dd72319e39e73311cf2f42a
Chocolate1999 <2816778271@qq.com>
2020-10-03 11:29:33 +0800

    React Router 后台动态获取路由（完结）

--- 学习React/React Router学习/demo02/src/AppRouter.js
+++ 学习React/React Router学习/demo02/src/AppRouter.js
@@ -1,31 +1,42 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Index from './Pages/Index';
-import './index.css';
-import Video from './Pages/Video';
-import Workplace from './Pages/WorkPlace';
+import Index from './Pages/Index'
+import Video from './Pages/Video'
+import Workplace from './Pages/WorkPlace'
+import './index.css'
 
 function AppRouter() {
+  let routeConfig = [
+    { path: '/', title: '博客首页', exact: true, component: Index },
+    { path: '/video/', title: '视频教程', exact: false, component: Video },
+    { path: '/workplace/', title: '职场技能', exact: false, component: Workplace }
+  ]
   return (
     <Router>
       <div className="mainDiv">
         <div className="leftNav">
           <h3>一级导航</h3>
           <ul>
-            <li> <Link to="/">博客首页</Link> </li>
-            <li><Link to="/video/">视频教程</Link> </li>
-            <li><Link to="/workplace">职场技能</Link> </li>
+            {
+              routeConfig.map((item, index) => {
+                return (<li key={index}> <Link to={item.path}>{item.title}</Link> </li>)
+              })
+            }
           </ul>
         </div>
 
         <div className="rightMain">
-          <Route path="/" exact component={Index} />
-          <Route path="/video/" component={Video} />
-          <Route path="/workplace/" component={Workplace} />
+          {
+            routeConfig.map((item, index) => {
+              return (<Route key={index} exact={item.exact} path={item.path} component={item.component} />)
+            })
+          }
+
         </div>
       </div>
     </Router>
   );
 }
+
 
 export default AppRouter;
