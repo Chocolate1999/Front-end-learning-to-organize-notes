d576b16e13e17f888d13ce04edb62a974b7b2bf6
Chocolate1999 <2816778271@qq.com>
2020-10-03 10:29:49 +0800

    TodoList组件改为UI无状态组件-提高性能

--- 学习React/Redux学习/demo02/src/TodoList.js
+++ 学习React/Redux学习/demo02/src/TodoList.js
@@ -1,26 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';  //引入连接器
 
-class TodoList extends Component {
-  render() {
-    let { inputValue, inputChange, clickButton, list } = this.props;
-    return (
+const TodoList =(props)=>{
+  let {inputValue ,inputChange,clickButton,list} = props; // 粘贴过来后，此处要进行修改
+  return (
       <div>
-        <div>
-          <input value={inputValue} onChange={inputChange} />
-          <button onClick={clickButton}>提交</button>
-        </div>
-        <ul>
-          {
-            list.map((item, index) => {
-              return (<li key={index}>{item}</li>)
-            })
-          }
-        </ul>
+          <div>
+              <input value={inputValue} onChange={inputChange} />
+              <button onClick={clickButton}>提交</button>
+          </div>
+          <ul>
+              {
+                  list.map((item,index)=>{
+                      return (<li key={index}>{item}</li>)
+                  })
+              }
+          </ul>
       </div>
-    );
-  }
+  );
 }
+
+// class TodoList extends Component {
+//   render() {
+//     let { inputValue, inputChange, clickButton, list } = this.props;
+//     return (
+//       <div>
+//         <div>
+//           <input value={inputValue} onChange={inputChange} />
+//           <button onClick={clickButton}>提交</button>
+//         </div>
+//         <ul>
+//           {
+//             list.map((item, index) => {
+//               return (<li key={index}>{item}</li>)
+//             })
+//           }
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
 
 // 映射关系就是把原来的state映射成组件中的props属性
 const stateToProps = (state) => {
