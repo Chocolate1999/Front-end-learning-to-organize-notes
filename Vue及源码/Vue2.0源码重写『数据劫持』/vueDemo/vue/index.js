e95fff7ba5a810a782503ef5d05ad181c8aabd34
Chocolate1999 <2816778271@qq.com>
2020-10-09 21:48:54 +0800

    学习Vue2.0数据劫持

---
+++ Vue及源码/Vue2.0源码重写『数据劫持』/vueDemo/vue/index.js
@@ -0,0 +1,1 @@
+console.log('Vue Use !');
---
+++ Vue及源码/Vue2.0源码重写『数据劫持』/vueDemo/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path'),
+      HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  // 配置入口
+  entry: './src/index.js',
+  // 配置输出
+  output: {
+    // 文件名称
+    filename: 'bundle.js',
+    // 配置路径
+    path: path.resolve(__dirname, 'dist')
+  },
+  // 配置 source-map ，打包出错定位到源码上
+  devtools: 'source-map',
+  resolve: {
+    modules: [path.resolve(__dirname, ''), path.resolve(__dirname, 'node_modules')]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html')
+    })
+  ]
+}
