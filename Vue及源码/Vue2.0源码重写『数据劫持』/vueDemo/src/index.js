0a1a9361a1ba50c48aedbccdcf72e013435a7c2e
Chocolate1999 <2816778271@qq.com>
2020-10-11 09:00:12 +0800

    Vue函数 / 初始化

--- Vue及源码/Vue2.0源码重写『数据劫持』/vueDemo/src/index.js
+++ Vue及源码/Vue2.0源码重写『数据劫持』/vueDemo/src/index.js
@@ -1,1 +1,23 @@
 import Vue from 'vue';
+// options
+let vm = new Vue({
+  el: '#app',
+  data() {
+    return {
+      title: '学生列表',
+      classNum: 1,
+      total: 2,
+      teachers: ['张三', '李四'],
+      students: [
+        {
+          id: 1,
+          name: '小红'
+        },
+        {
+          id: 2,
+          name: '小明'
+        }
+      ]
+    }
+  }
+})
--- Vue及源码/Vue2.0源码重写『数据劫持』/vueDemo/vue/index.js
+++ Vue及源码/Vue2.0源码重写『数据劫持』/vueDemo/vue/index.js
@@ -1,1 +1,14 @@
-console.log('Vue Use !');
+function Vue(options) {
+  this._init(options);
+}
+
+// 初始化
+Vue.prototype._init = function (options) {
+  // 保存实例
+  var vm = this;
+  vm.$options = options; // 将 options 挂载到实例上
+
+  initState(vm); // 初始化状态
+}
+
+export default Vue;
