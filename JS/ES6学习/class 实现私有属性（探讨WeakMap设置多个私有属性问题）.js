a15719ad7f8fa213ca0586d5b1bf9ea8d0cdf4f4
Chocolate1999 <2816778271@qq.com>
2020-10-12 14:21:38 +0800

    class 实现私有属性（探讨WeakMap设置多个私有属性问题）

--- JS/ES6学习/class 实现私有属性（探讨WeakMap设置多个私有属性问题）.js
+++ JS/ES6学习/class 实现私有属性（探讨WeakMap设置多个私有属性问题）.js
@@ -1,17 +1,13 @@
 /* WeakMap  */
 const classE = (function () {
   const _x = new WeakMap();
-  const _y = new WeakMap();
   class ClassE {
     constructor(x, y) {
       _x.set(this, x);
-      _y.set(this, y);
+      _x.set(this, y);
     }
     getX() {
       return _x.get(this);;
-    }
-    getY() {
-      return _y.get(this);;
     }
   }
   return ClassE;
@@ -19,8 +15,4 @@ const classE = (function () {
 
 let classe = new classE(5, 6);
 /* 此时不可以访问我们自定义私有属性命名的_x */
-console.log(classe._x); // undefined
-console.log(classe.getX()); // 5
-
-console.log(classe._y); // undefined
-console.log(classe.getY()); // 6
+console.log(classe.getX()); // 6
