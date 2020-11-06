c42469ffebe49750d8aa9398de347b53277160ce
Chocolate1999 <2816778271@qq.com>
2020-11-06 21:24:47 +0800

    浏览器中的Event loop

---
+++ JS/事件循环/浏览器中的Event loop.js
@@ -0,0 +1,48 @@
+console.log('script start')
+
+async function async1() {
+  await async2()
+  console.log('async1 end')
+}
+async function async2() {
+  console.log('async2 end')
+}
+async1()
+
+setTimeout(function() {
+  console.log('setTimeout')
+}, 0)
+
+new Promise(resolve => {
+  console.log('Promise')
+  resolve()
+})
+  .then(function() {
+    console.log('promise1')
+  })
+  .then(function() {
+    console.log('promise2')
+  })
+
+console.log('script end')
+
+/**
+ * script start
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// script start => async2 end => Promise => script end => promise1 => promise2 => async1 end => setTimeout
