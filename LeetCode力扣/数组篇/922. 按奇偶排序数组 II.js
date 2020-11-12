3818b785d39af6d684d2af03f6bb2943a94339c6
Chocolate1999 <2816778271@qq.com>
2020-11-12 20:49:58 +0800

    add 922. 按奇偶排序数组 II

--- LeetCode力扣/数组篇/922. 按奇偶排序数组 II.js
+++ LeetCode力扣/数组篇/922. 按奇偶排序数组 II.js
@@ -3,25 +3,25 @@
  * @return {number[]}
  */
 var sortArrayByParityII = function (A) {
-  let ans = [];
+  let ans = []; // 输出结果数组
   let len = A.length;
   for (let i = 0; i < len; i++) {
-      if (i & 1) {
+      if (i & 1) { //拿到奇数项
           let tmp;
           while (1) {
               tmp = A.shift();
               if (tmp & 1) break;
               else A.push(tmp);
           }
-          ans[i] = tmp;
-      } else {
+          ans[i] = tmp; // 存放当前奇数下标的奇数项
+      } else { // 拿到偶数项
           let tmp;
           while (1) {
               tmp = A.shift();
               if (tmp % 2 == 0) break;
               else A.push(tmp);
           }
-          ans[i] = tmp;
+          ans[i] = tmp; // 存放当前偶数下标的偶数项
       }
   }
   return ans;
