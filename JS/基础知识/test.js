e71797b58217a95822fb9a0dcefecdbc458f5d82
Chocolate1999 <2816778271@qq.com>
2020-11-24 22:11:08 +0800

    update

--- JS/基础知识/test.js
+++ JS/基础知识/test.js
@@ -1,28 +1,9 @@
-let obj = [{
-  'name': 'Chocolate',
-  'age': 21,
-  'sex': 'm'
-}, {
-  'name': 'ycy',
-  'age': 21
-}]
-
-// for (let key in obj) {
-//   console.log(key);
-// }
-
-// for (let key of obj) {
-//   console.log(key);
-// }
-
-// let r = { 'a': { 'name':111}, 'b': { } }
-// for (let key in r) {
-//   console.log(r[key]);
-// }
-
-// console.log(r.a);
-
-
-for(let key in 'yangchaoyi'){
-  console.log(key);
+function foo(){
+  var a = 1;
+  let b = 2;
+  {
+    console.log(a);
+    console.log(b);
+  }
 }
+foo();
