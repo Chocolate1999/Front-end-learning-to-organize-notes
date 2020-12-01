20dc7f3b78cb7b912a160a53db0ed6bf319bc8cf
Chocolate1999 <2816778271@qq.com>
2020-12-01 19:53:11 +0800

    array.forEach() 方法

---
+++ 写给女友/数组一系列操作/数组遍历/array.forEach() 方法.js
@@ -0,0 +1,19 @@
+// let arr = ['Chocolate', 'zhlll', 'lionkk'];
+
+// arr.forEach(function (cur, index, arr) {
+//   console.log(cur, index, arr);
+// })
+
+// Chocolate 0 [ 'Chocolate', 'zhlll', 'lionkk' ]
+// zhlll 1 [ 'Chocolate', 'zhlll', 'lionkk' ]
+// lionkk 2 [ 'Chocolate', 'zhlll', 'lionkk' ]
+
+
+let arr = ['Chocolate', 'zhlll', 'lionkk'];
+
+arr.forEach((cur) => {
+  console.log(cur);
+})
+// Chocolate
+// zhlll
+// lionkk
