e2f1d1f11c592e8cc372c54b9229d65bce6a0a2b
Chocolate1999 <2816778271@qq.com>
2020-12-01 22:58:54 +0800

    splice()

---
+++ 写给女友/数组一系列操作/改变原数组的方法/splice().js
@@ -0,0 +1,10 @@
+// let arr = ['one', 'two', 'three'];
+
+// arr.splice(2, 0, 'Chocolate');
+// console.log(arr);// [ 'one', 'two', 'Chocolate', 'three' ]
+
+
+let arr = ['one', 'two', 'three'];
+
+arr.splice(2, 1, 'Chocolate');
+console.log(arr);// [ 'one', 'two', 'Chocolate' ]
