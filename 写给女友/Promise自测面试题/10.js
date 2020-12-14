9c4112115455a4c0749a00d42a87238f3909cb67
Chocolate1999 <2816778271@qq.com>
2020-12-14 22:33:58 +0800

    第十题

---
+++ 写给女友/Promise自测面试题/10.js
@@ -0,0 +1,30 @@
+let a;
+
+const b = new Promise((resolve, reject) => {
+  console.log('promise1');
+  resolve();
+}).then(() => {
+  console.log('promise2');
+}).then(() => {
+  console.log('promise3');
+}).then(() => {
+  console.log('promise4');
+});
+
+a = new Promise(async (resolve, reject) => {
+  console.log(a);
+  await b;
+  console.log(a);
+  console.log('after1');
+  await a;
+  resolve(true);
+  console.log('after2');
+})
+
+console.log('end');
+
+<details><summary><b>查看答案</b></summary>
+<p>
+
+</p>
+</details>
