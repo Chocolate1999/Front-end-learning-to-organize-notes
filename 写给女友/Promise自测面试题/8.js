2d6b9e32c4d8d165ccdc3abb015d97cc7338354d
Chocolate1999 <2816778271@qq.com>
2020-12-14 20:03:25 +0800

    第九题

--- 写给女友/Promise自测面试题/8.js
+++ 写给女友/Promise自测面试题/8.js
@@ -1,10 +1,27 @@
+// async function async1() {
+//   await async2();
+//   console.log('async1 end');
+// }
+
+// async function async2() {
+//   console.log('async2 end');
+// }
+// async1();
+// console.log(10);
+
+
 async function async1() {
-  await async2();
-  console.log('async1 end');
+  try {
+    await async2();
+  } catch (err) {
+    console.log('async1 end');
+    console.log(err);
+  }
 }
 
 async function async2() {
   console.log('async2 end');
+  return Promise.reject(new Error('error!!!'));
 }
 async1();
 console.log(10);
---
+++ 写给女友/Promise自测面试题/9.js
@@ -0,0 +1,20 @@
+async function async1() {
+  console.log('async1 start');
+  await async2();
+  console.log('async1 end');
+}
+async function async2() {
+  console.log('async2');
+}
+console.log('script start');
+setTimeout(function() {
+  console.log('setTimeout');
+}, 0)
+async1();
+new Promise(function(resolve) {
+  console.log('promise1');
+  resolve();
+}).then(function() {
+  console.log('promise2');
+});
+console.log('script end');
