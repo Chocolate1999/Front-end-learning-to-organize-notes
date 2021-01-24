05c5a14ba0973b8f7d3bce8b0bd287fa7f5f89df
Chocolate1999 <2816778271@qq.com>
2021-01-24 20:21:54 +0800

    JavaScript深入之call的模拟实现.js

---
+++ 公众号：小狮子前端/JS一系列问题/JavaScript深入之call的模拟实现.js
@@ -0,0 +1,29 @@
+// 第二版
+Function.prototype.call2 = function (context) {
+    // console.log(context); // fpp
+    context.fn = this;
+    // console.log(this); // bar:function
+    var args = [];
+    for (var i = 1, len = arguments.length; i < len; i++) {
+        args.push('arguments[' + i + ']');
+    }
+    let result = context.fn(...args);
+    delete context.fn;
+    return result;
+}
+
+// 测试一下
+var foo = {
+    value: 1
+};
+
+function bar(name, age) {
+    console.log(name)
+    console.log(age)
+    console.log(this.value);
+}
+
+bar.call2(foo, 'kevin', 18);
+// kevin
+// 18
+// 1
