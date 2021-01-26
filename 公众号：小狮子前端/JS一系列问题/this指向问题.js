1b148ed4c002d37f3256eaa9a3c5d413d2193351
Chocolate1999 <2816778271@qq.com>
2021-01-26 22:05:47 +0800

    this指向问题

--- 公众号：小狮子前端/JS一系列问题/this指向问题.js
+++ 公众号：小狮子前端/JS一系列问题/this指向问题.js
@@ -6,4 +6,16 @@ var obj = {
   msg: 'aaa'
 }
 var res = test(obj);
-console.log(res);
+console.log(res); // 'Chocolate'
+
+
+
+// let msg = 'Chocolate';
+// function test() {
+//   return this.msg;
+// }
+// var obj = {
+//   msg: 'aaa'
+// }
+// var res = test(obj);
+// console.log(res); // 'undefined'
